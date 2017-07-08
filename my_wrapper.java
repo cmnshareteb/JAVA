54b6224bedcb235f3b9edd316d4e08a33f0ac83d
cmnshareteb <cmnshareteb@gmail.com>
2017-07-08 22:05:49 +0530

    Add files via upload

---
+++ my_wrapper.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+
+class my_wrapper
+{
+	public static void main(String args[])
+	{
+		Integer num1=new Integer(200);
+		int i=num1.intValue();
+		
+		System.out.println("num1="+num1+"\ni="+i);
+	}
+}
